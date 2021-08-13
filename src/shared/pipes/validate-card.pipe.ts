cb281795bd2793ba8391b63fb8461f7909d7a7f0
Manoj Kelath <manojkelath1987@gmail.com>
2021-08-12 23:43:52 -0400

    add sample pipe

---
+++ src/shared/pipes/validate-card.pipe.ts
@@ -0,0 +1,38 @@
+
+
+import { PipeTransform, ArgumentMetadata, BadRequestException, HttpStatus, Injectable } from '@nestjs/common';
+import { validate } from 'class-validator';
+
+import { HttpException } from '@nestjs/common/exceptions/http.exception';
+
+@Injectable()
+export class ValidateCardPipe implements PipeTransform<any> {
+    async transform(cardNumber, metadata: ArgumentMetadata) {
+
+        if (!cardNumber) {
+            throw new BadRequestException('No card submitted');
+        }
+
+        cardNumber = cardNumber.split(' ').join("");
+        if (parseInt(cardNumber) <= 0 || (!/\d{15,16}(~\W[a-zA-Z])*$/.test(cardNumber)) || cardNumber.length > 16) {
+            return false;
+        }
+        var carray = new Array();
+        for (var i = 0; i < cardNumber.length; i++) {
+            carray[carray.length] = cardNumber.charCodeAt(i) - 48;
+        }
+        carray.reverse();
+        var sum = 0;
+        for (var i = 0; i < carray.length; i++) {
+            var tmp = carray[i];
+            if ((i % 2) != 0) {
+                tmp *= 2;
+                if (tmp > 9) {
+                    tmp -= 9;
+                }
+            }
+            sum += tmp;
+        }
+        return ((sum % 10) == 0);
+    }
+}
