8e974e286507a6584200f396ed1a79aa9b59b680
Manoj Kelath <manojkelath1987@gmail.com>
2021-08-18 19:07:29 -0400

    make user id in wallet entity FK

--- src/modules/wallet/wallet.entity.ts
+++ src/modules/wallet/wallet.entity.ts
@@ -1,5 +1,6 @@
+import { UserEntity } from './../user/user.entity';
 import { IsEmail } from 'class-validator';
-import { Entity, PrimaryGeneratedColumn, Column } from 'typeorm';
+import { Entity, PrimaryGeneratedColumn, Column, TableExclusion, TableColumn, OneToOne, JoinColumn } from 'typeorm';
 
 @Entity('wallet')
 export class WalletEntity {
@@ -7,7 +8,8 @@ export class WalletEntity {
     @PrimaryGeneratedColumn()
     id: number;
 
-    @Column()
+    @OneToOne(type => UserEntity, user => user.id)
+    @JoinColumn({ name: "userId" })  // This matches @PrimaryColumn name
     userId: string;
 
     @Column()
